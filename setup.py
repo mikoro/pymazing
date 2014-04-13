ae315c6863246a879efe378db2f32fb06bde7f11
Mikko Ronkainen <mikko@mikkoronkainen.com>
2014-04-13 15:01:35 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {"packages": ["numbers", "OpenGL"], "excludes": ["sfml"], "include_msvcr": "true"}
-                
+
+base = None
+
+if sys.platform == "win32":
+    base = "Win32GUI"
+
 setup(name="pymazing",
       version="0.1.0",
       options = {"build_exe": build_exe_options},
-      executables = [Executable("pymazing.py", base="Win32GUI")])
+      executables = [Executable("pymazing.py", base=base)])
 
 # after build, copy data (from project) and smfl (from site-packages) folders to the build folder
